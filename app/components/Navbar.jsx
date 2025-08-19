5a49b33d01fe7da52b87681c2dfa3d2ecc3c5c8f
ashuOO7 <ashjnavj@gmail.com>
2025-08-19 15:44:24 +0530

    making app responsive

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,22 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMALIZER</p>
-            </Link>
-            <div>
-                <Link to="/upload" className="green-button w-fit">
-                    Upload Resume
-                </Link>
-                <Link to="/wipe" className="red-button w-fit ml-2">
-                    Delete resumes
-                </Link>
-            </div>
-        </nav>
-    )
-}
-export default Navbar
+  return (
+    <nav className="navbar flex flex-col sm:flex-row items-center justify-between gap-4 px-4 py-3">
+      <Link to="/" className="flex-shrink-0">
+        <p className="text-2xl font-bold text-gradient">RESUMALIZER</p>
+      </Link>
+
+      <div className="flex flex-row items-center gap-2 w-full sm:w-auto justify-center sm:justify-end">
+        <Link to="/upload" className="green-button w-fit text-center">
+          Upload Resume
+        </Link>
+        <Link to="/wipe" className="red-button w-fit text-center">
+          Delete resumes
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
